42c8dbe35ac389da5a92bf93b4f275c5e21b6065
Ubuntu <ubuntu@ip-172-31-41-223.ap-northeast-2.compute.internal>
2025-01-16 01:41:24 +0000

    at ubuntu

--- api/apps.py
+++ api/apps.py
@@ -13,5 +13,5 @@ class ApiConfig(AppConfig):
         Callback 메소드로 Django 애플리케이션이 준비되었을 때 호출된다.
         python-dotenv 패키지의 load_dotenv() 함수를 사용하여 .env 파일에서 시스템 환경 변수로 로드한다.
         """
-        r = load_dotenv(BASE_DIR / ".env")
+        r = load_dotenv(BASE_DIR / ".env").get("OPEN_API_KEY")
         print("OPENAPI KEY Load:", r)
